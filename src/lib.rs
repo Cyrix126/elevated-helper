b03b6eb496ed2ca3babd52048897c18483bd4343
Cyrix126 <58007246+Cyrix126@users.noreply.github.com>
2026-02-28 17:19:56 +0100

    feat: start process with pty

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,11 @@
 use std::{
     io::{BufRead, BufReader, Read, Write},
-    process::{Command, Stdio},
+    process::Command,
     thread,
 };
 
-use interprocess::{
-    TryClone,
-    os::windows::named_pipe::{DuplexPipeStream, pipe_mode},
-};
+use conpty::Process;
+use interprocess::os::windows::named_pipe::{DuplexPipeStream, pipe_mode};
 
 pub use interprocess;
 
@@ -18,41 +16,30 @@ pub fn run(args: Args) -> std::io::Result<()> {
     let stdin_pipe: DuplexPipeStream<pipe_mode::Bytes> =
         DuplexPipeStream::connect_by_path(format!(r"\\.\pipe\{}", args.name_pipe_stdin))?;
 
-    let stdout_pipe: DuplexPipeStream<pipe_mode::Bytes> =
+    let mut stdout_pipe: DuplexPipeStream<pipe_mode::Bytes> =
         DuplexPipeStream::connect_by_path(format!(r"\\.\pipe\{}", args.name_pipe_stdout))?;
 
-    let mut child = Command::new(&args.program_path)
-        .args(&args.program_args)
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .spawn()?;
+    let mut cmd = Command::new(&args.program_path);
+    cmd.args(&args.program_args);
+    let mut child = Process::spawn(cmd)?;
 
-    let process_stdin = child.stdin.take().unwrap();
-    let process_stdout = child.stdout.take().unwrap();
-    let process_stderr = child.stderr.take().unwrap();
+    let process_stdin = child.input().unwrap();
+    let process_stdout = child.output().unwrap();
 
     let stdin_handle = thread::spawn(move || {
         bridge_input(stdin_pipe, process_stdin);
     });
 
-    let mut stdout_pipe_clone = stdout_pipe.try_clone().unwrap();
     let stdout_handle = thread::spawn(move || {
-        bridge_output(process_stdout, &mut stdout_pipe_clone);
+        bridge_output(process_stdout, &mut stdout_pipe);
     });
 
-    let mut stderr_pipe = stdout_pipe;
-    let stderr_handle = thread::spawn(move || {
-        bridge_output(process_stderr, &mut stderr_pipe);
-    });
-
-    let status = child.wait()?;
+    let status = child.wait(None)?;
 
     stdin_handle.join().ok();
     stdout_handle.join().ok();
-    stderr_handle.join().ok();
 
-    std::process::exit(status.code().unwrap_or(1));
+    std::process::exit(status as i32);
 }
 
 fn bridge_input(mut from: DuplexPipeStream<pipe_mode::Bytes>, mut to: impl Write) {
